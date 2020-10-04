71d9d6c0380aa41b3cb45d2e58ed9f608e546ed3
Swackles <Swackles@users.noreply.github.com>
2020-10-04 19:54:38 +0300

    shows version as activity

--- src/util/events/ready.ts
+++ src/util/events/ready.ts
@@ -1,10 +1,13 @@
 import client from './../../index'
 
+const pkg = require('../../../package.json');
+
 /**
  * Controller for client ready event
  */
 async function controller() {
   console.log(`Logged in as ${client.user.tag}!`);
+  await client.user.setActivity(`v${pkg.version}`, { type: 'PLAYING' })
 }
 
 export default controller
